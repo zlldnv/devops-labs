4c1cf5358041da86e45dea2b43f3d0924882ddd2
Anton Zalaldinov <v.anton.zalaldinov@reddit.com>
2022-06-07 08:10:36 +0300

    add terraform file

--- app_python/terraform/github/main.tf
+++ app_python/terraform/github/main.tf
@@ -10,3 +10,31 @@ terraform {
 provider "github" {
   # Configuration options
 }
+
+
+resource "github_repository" "devops-labs" {
+  name        = "devops-labs"
+  description = "Solutions to DevOps labs"
+  visibility = "public"
+  auto_init = true
+}
+
+
+resource "github_branch_default" "default"{
+    repository = github_repository.devops-labs.name
+    branch     = master
+}
+
+resource "github_branch" "lab4" {
+    repository = github_repository.devops-labs.name
+    branch     = "lab4"
+}
+
+resource "github_branch_protection_v3" "protection" {
+  repository     = github_repository.devops-labs.name
+  branch         = "lab4"
+
+  restrictions {
+    users = ["zlldnv"]
+  }
+}
